0bab2fe4f930820ab108927e232558d7657b7b1e
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-05-27 13:52:05 -0500

    Correcion de Validaciones P1

--- orderweb/resources/views/causal/index.blade.php
+++ orderweb/resources/views/causal/index.blade.php
@@ -1,15 +1,13 @@
 @extends('templates.base')
-@section('title', 'Causal')
-@section('header', 'Causal')
-@section('content')
-    
-    
+@section('title', 'Causales')
+@section('header', 'Causales')
+@section('content')    
+
     <div class="row">
         <div class="col-lg-12 mb-4 d-grid gap-2 d-md-block">
             <a href="{{ route('causal.create') }}" class="btn btn-primary">Crear</a>
         </div>
     </div>
-
 
     @include('templates.messages')
 
@@ -24,31 +22,29 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($causals as $causal)
-                    
-                    <tr>
-                        <td>{{ $causal["id"] }}</td>
-                        <td>{{ $causal["description"] }}</td>
-                        <td>
-                            <a href="{{ route('causal.edit',$causal["id"]) }}" title="editar" class="btn btn-primary btn-circle btn-sm">
-                                <i class="far fa-edit"></i>
-                            </a>
-                            <a href="{{ route('causal.destroy',$causal["id"]) }}" title="eliminar" class="btn btn-danger btn-circle btn-sm"
-                            onclick="return remove();">
-                            <i class="fas fa-trash"></i>
-                        </a>
-                    </td>
-                </tr>
-                @endforeach
+                    @foreach ($causals as $causal) 
+                        <tr>
+                            <td>{{ $causal["id"] }}</td>
+                            <td>{{ $causal["description"] }}</td>
+                            <td>
+                                <a href="{{ route('causal.edit', $causal["id"]) }}" class="btn btn-primary btn-circle btn-sm" title="Editar">
+                                    <i class="far fa-edit"></i>
+                                </a>
+                                <a href="{{ route('causal.destroy', $causal["id"]) }}" class="btn btn-danger btn-circle btn-sm" title="Eliminar" 
+                                    onclick="return remove();">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
+    
 
 @endsection
 
 @section('scripts')
-
     <script src="{{ asset('js/general.js') }}"></script>
-
 @endsection
